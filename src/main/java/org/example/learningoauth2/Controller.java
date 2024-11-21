251016441c1f1b76ad218b1cc0369df29eb2ac63
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-11-21 20:55:53 -0300

    feat: method jwt

--- src/main/java/org/example/learningoauth2/Controller.java
+++ src/main/java/org/example/learningoauth2/Controller.java
@@ -1,8 +1,12 @@
 package org.example.learningoauth2;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api")
@@ -10,11 +14,31 @@ public class Controller {
 
     @GetMapping("/public")
     public String publicHello() {
-        return "Hello, i'm public!";
+        return "<h1>Hello, i'm public!</h1> <br> Thanks for visiting!";
     }
 
-    @GetMapping("/private")
-    public String privateHello() {
-        return "Hello, i'm private!";
+    //Esse método retorna as informações so usuário logado
+    @GetMapping("/cookie")
+    public String privateHello(@AuthenticationPrincipal OidcUser principal) {
+
+        return String.format("""
+                <h1>Hello, i'm private!</h1> <br> Thanks for visiting
+                
+                Principal: %s
+                Email attribute: %s
+                Authorities: %s
+                JWT: %s
+                """, principal, principal.getAttribute("email"), principal.getAuthorities(),
+                principal.getIdToken().getTokenValue());
+    }
+
+    @GetMapping("/jwt")
+    public String jwt(@AuthenticationPrincipal Jwt jwt) {
+        return String.format("""
+                <h1>JWT</h1>
+               Principal: %s/n
+                Email attribute: %s/n
+                JWT: %s/n
+               """, jwt.getClaims(), jwt.getClaim("email"), jwt.getTokenValue());
     }
 }
